85a1d994129ed54cef93a764c699e0bc6117e2ab
Giuseppe Digilio <giuseppe.digilio@4science.it>
2020-03-10 15:32:26 +0100

    Added WorkpackageStatusType

---
+++ src/app/core/working-plan/models/workpackage-status-type.ts
@@ -0,0 +1,8 @@
+export enum WorkpackageStatusType {
+  done = 'done',
+  additional = 'additional',
+  overdue = 'overdue',
+  not_done = 'not_done',
+  changed = 'changed',
+  removed = 'removed'
+}
