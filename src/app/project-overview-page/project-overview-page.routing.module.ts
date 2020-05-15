a1f67ab057ab37b096b04d229886884f1231ccb4
Giuseppe Digilio <giuseppe.digilio@4science.it>
2020-05-15 14:41:45 +0200

    [CST-2656] Implemented project breadcrumbs

--- src/app/project-overview-page/project-overview-page.routing.module.ts
+++ src/app/project-overview-page/project-overview-page.routing.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { ProjectOverviewPageComponent } from './project-overview-page.component';
+import { I18nBreadcrumbResolver } from '../core/breadcrumbs/i18n-breadcrumb.resolver';
+import { I18nBreadcrumbsService } from '../core/breadcrumbs/i18n-breadcrumbs.service';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
       {
         path: '',
+        resolve: { breadcrumb: I18nBreadcrumbResolver },
+        data: { title: 'project-overview.page.title', breadcrumbKey: 'project-overview' },
         component: ProjectOverviewPageComponent,
         pathMatch: 'full',
-        data: { title: 'project-overview.page.title' }
       }
     ]),
   ],
-  declarations: []
+  providers: [
+    I18nBreadcrumbResolver,
+    I18nBreadcrumbsService
+  ]
 })
 export class ProjectOverviewPageRoutingModule {
 }
