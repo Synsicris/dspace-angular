fb7468c5a8e7158990ccf8493ac02a7af835960e
Giuseppe Digilio <giuseppe.digilio@4science.it>
2020-08-25 19:00:47 +0200

    [CST-3226] added environment variables for projects

--- src/environments/environment.common.ts
+++ src/environments/environment.common.ts
@@ -258,5 +258,9 @@ export const environment: GlobalConfig = {
     workpackageStatusTypeAuthority: 'working_plan_workpackage_status_type',
     workpackageStepTypeAuthority: 'working_plan_workpackage_step_type',
     workingPlanPlaceMetadata: 'workingplan.place'
+  },
+  projects: {
+    projectTemplateUUID: '',
+    communityProjectsUUID: ''
   }
 };
