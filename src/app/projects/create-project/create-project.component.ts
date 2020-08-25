dbc620906b424deecadc4cf83153016a8fa4dfac
Giuseppe Digilio <giuseppe.digilio@4science.it>
2020-08-25 19:06:04 +0200

    [CST-3226] Implemented component for project creation

--- src/app/projects/create-project/create-project.component.ts
+++ src/app/projects/create-project/create-project.component.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import { Observable, of as observableOf } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
-import {
-  COLLECTION_PARENT_PARAMETER,
-  getCollectionCreatePath
-} from '../../+collection-page/collection-page-routing.module';
 import { DSpaceObject } from '../../core/shared/dspace-object.model';
-import { ImpactPathwayService } from '../../core/impact-pathway/impact-pathway.service';
 import { NotificationsService } from '../../shared/notifications/notifications.service';
+import { ProjectDataService } from '../../core/project/project-data.service';
+import { RemoteData } from '../../core/data/remote-data';
+import { Community } from '../../core/shared/community.model';
+import { RequestService } from '../../core/data/request.service';
+import { getFirstSucceededRemoteDataPayload } from '../../core/shared/operators';
 
 /**
- * Component to wrap a list of existing communities inside a modal
- * Used to choose a community from to create a new collection in
+ * Component to wrap a form inside a modal
+ * Used to create a new project
  */
-
 @Component({
   selector: 'ds-create-project-parent-selector',
   templateUrl: './create-project.component.html',
@@ -29,48 +29,74 @@ export class CreateProjectComponent implements OnInit {
    */
   public createForm: FormGroup;
 
-  public processing$: Observable<boolean> = observableOf(false);
+  public processing$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
+  /**
+   * Initialize instance variables
+   *
+   * @param {NgbActiveModal} activeModal
+   * @param {FormBuilder} formBuilder
+   * @param {NotificationsService} notificationService
+   * @param {ProjectDataService} projectService
+   * @param {RequestService} requestService
+   * @param {ActivatedRoute} route
+   * @param {Router} router
+   * @param {TranslateService} translate
+   */
   constructor(
     private activeModal: NgbActiveModal,
     private formBuilder: FormBuilder,
-    private impactPathwayService: ImpactPathwayService,
     private notificationService: NotificationsService,
+    private projectService: ProjectDataService,
+    private requestService: RequestService,
     private route: ActivatedRoute,
-    private router: Router) {
+    private router: Router,
+    private translate: TranslateService) {
   }
 
+  /**
+   * Initialize the component, setting up creation form
+   */
   ngOnInit(): void {
-    this.processing$ = this.impactPathwayService.isProcessing();
-
     this.createForm = this.formBuilder.group({
       name: ['', Validators.required]
     });
   }
 
   /**
-   * Close the modal
+   * Close the active modal
    */
   close() {
     this.createForm.reset();
     this.activeModal.close();
   }
 
+  /**
+   * Dispatch new project creation
+   */
   create() {
-    const impactPathwayName = this.createForm.get('name').value;
-    this.impactPathwayService.dispatchGenerateImpactPathway(impactPathwayName);
+    this.processing$.next(true);
+    const projectName = this.createForm.get('name').value;
+    this.projectService.createProject(projectName)
+      .subscribe((response: RemoteData<Community>) => {
+        if (response.hasSucceeded) {
+          this.navigate(response.payload);
+          response.payload.parentCommunity.pipe(getFirstSucceededRemoteDataPayload())
+            .subscribe((parent: Community) => {
+              this.notificationService.success(null, this.translate.instant('project.create.success'));
+              this.requestService.removeByHrefSubstring(parent._links.self.href);
+            });
+        }
+        this.processing$.next(false);
+        this.close();
+    });
   }
 
   /**
    * Navigate to the collection create page
    */
   navigate(dso: DSpaceObject) {
-    const navigationExtras: NavigationExtras = {
-      queryParams: {
-        [COLLECTION_PARENT_PARAMETER]: dso.uuid,
-      }
-    };
-    this.router.navigate([getCollectionCreatePath()], navigationExtras);
+    this.router.navigate(['project-overview', dso.id]);
   }
 
 }
