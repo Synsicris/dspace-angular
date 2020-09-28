a009cfe3ba9e801d9614eb4df27bab49ecabd418
Giuseppe Digilio <giuseppe.digilio@4science.it>
2020-09-28 18:21:41 +0200

    [CST-3417] Added find by item method to workspaceitem-data.service.ts

--- src/app/core/submission/workspaceitem-data.service.ts
+++ src/app/core/submission/workspaceitem-data.service.ts
@@ -12,6 +12,15 @@ import { NotificationsService } from '../../shared/notifications/notifications.s
 import { ObjectCacheService } from '../cache/object-cache.service';
 import { DSOChangeAnalyzer } from '../data/dso-change-analyzer.service';
 import { WorkspaceItem } from './models/workspaceitem.model';
+import { FindListOptions } from '../data/request.models';
+import { FollowLinkConfig } from '../../shared/utils/follow-link-config.model';
+import { RequestParam } from '../cache/models/request-param.model';
+import { getFinishedRemoteData } from '../shared/operators';
+import { RemoteData } from '../data/remote-data';
+import { PaginatedList } from '../data/paginated-list';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs/internal/Observable';
+import { createFailedRemoteDataObject, createSuccessfulRemoteDataObject } from '../../shared/remote-data.utils';
 
 /**
  * A service that provides methods to make REST requests with workspaceitems endpoint.
@@ -20,6 +29,7 @@ import { WorkspaceItem } from './models/workspaceitem.model';
 @dataService(WorkspaceItem.type)
 export class WorkspaceitemDataService extends DataService<WorkspaceItem> {
   protected linkPath = 'workspaceitems';
+  protected searchByItemLinkPath = 'item';
 
   constructor(
     protected comparator: DSOChangeAnalyzer<WorkspaceItem>,
@@ -33,4 +43,26 @@ export class WorkspaceitemDataService extends DataService<WorkspaceItem> {
     super();
   }
 
+  /**
+   * Return the WorkspaceItem object found through the UUID of an item
+   *
+   * @param uuid           The uuid of the item
+   * @param options        The {@link FindListOptions} object
+   * @param linksToFollow  List of {@link FollowLinkConfig} that indicate which {@link HALLink}s should be automatically resolved
+   */
+  public findByItem(uuid: string, options: FindListOptions = {}, ...linksToFollow: Array<FollowLinkConfig<WorkspaceItem>>): Observable<RemoteData<WorkspaceItem>> {
+    const optionsWithUUID = Object.assign(new FindListOptions(), options, {
+      searchParams: [new RequestParam('uuid', uuid)]
+    });
+    return this.searchBy(this.searchByItemLinkPath, optionsWithUUID, ...linksToFollow).pipe(
+      getFinishedRemoteData(),
+      map((rd: RemoteData<PaginatedList<WorkspaceItem>>) => {
+        if (rd.hasSucceeded) {
+          return createSuccessfulRemoteDataObject(rd.payload.page[0]);
+        } else {
+          return createFailedRemoteDataObject(null, rd.error);
+        }
+      })
+    );
+  }
 }
