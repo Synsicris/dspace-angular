7bcd1835d0b43e8e0aa00476402ab30c0c1cdbc1
Giuseppe Digilio <giuseppe.digilio@4science.it>
2021-03-02 20:45:49 +0100

    [CST-3708] add edit button when listing items in the mydspace results

--- src/app/shared/mydspace-actions/item/item-actions.component.ts
+++ src/app/shared/mydspace-actions/item/item-actions.component.ts
@@ -1,6 +1,8 @@
 import { Component, Injector, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { BehaviorSubject, Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 import { TranslateService } from '@ngx-translate/core';
 
 import { MyDSpaceActionsComponent } from '../mydspace-actions';
@@ -9,6 +11,8 @@ import { Item } from '../../../core/shared/item.model';
 import { NotificationsService } from '../../notifications/notifications.service';
 import { RequestService } from '../../../core/data/request.service';
 import { SearchService } from '../../../core/shared/search/search.service';
+import { EditItemMode } from '../../../core/submission/models/edititem-mode.model';
+import { EditItemDataService } from '../../../core/submission/edititem-data.service';
 
 /**
  * This component represents mydspace actions related to Item object.
@@ -27,6 +31,11 @@ export class ItemActionsComponent extends MyDSpaceActionsComponent<Item, ItemDat
   @Input() object: Item;
 
   /**
+   * A boolean representing if component is redirecting to edit page
+   * @type {BehaviorSubject<boolean>}
+   */
+  public isRedirectingToEdit$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  /**
    * Initialize instance variables
    *
    * @param {Injector} injector
@@ -35,13 +44,15 @@ export class ItemActionsComponent extends MyDSpaceActionsComponent<Item, ItemDat
    * @param {TranslateService} translate
    * @param {SearchService} searchService
    * @param {RequestService} requestService
+   * @param {EditItemDataService} editItemDataService
    */
   constructor(protected injector: Injector,
               protected router: Router,
               protected notificationsService: NotificationsService,
               protected translate: TranslateService,
               protected searchService: SearchService,
-              protected requestService: RequestService) {
+              protected requestService: RequestService,
+              protected editItemDataService: EditItemDataService) {
     super(Item.type, injector, router, notificationsService, translate, searchService, requestService);
   }
 
@@ -54,4 +65,26 @@ export class ItemActionsComponent extends MyDSpaceActionsComponent<Item, ItemDat
     this.object = object;
   }
 
+  /**
+   * Check if edit modes are available for the item
+   */
+  canEdit(): Observable<boolean> {
+    return this.editItemDataService.searchEditModesByID(this.object.id).pipe(
+      map((editModes: EditItemMode[]) => editModes && editModes.length > 0)
+    );
+  }
+
+  /**
+   * Navigate to edit item page
+   */
+  public navigateToEditItemPage(): void {
+    this.isRedirectingToEdit$.next(true);
+    this.editItemDataService.searchEditModesByID(this.object.id).pipe(
+      filter((editModes: EditItemMode[]) => editModes && editModes.length > 0),
+      map((editModes: EditItemMode[]) => editModes[0])
+    ).subscribe((editMode: EditItemMode) => {
+      this.router.navigate(['edit-items', this.object.id + ':' + editMode.name]);
+      this.isRedirectingToEdit$.next(false);
+    });
+  }
 }
