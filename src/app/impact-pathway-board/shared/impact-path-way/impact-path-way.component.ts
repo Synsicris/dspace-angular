526d03bd1cd81d42776e41e6a60108170fb6fd3b
Giuseppe Digilio <giuseppe.digilio@4science.it>
2021-04-01 10:46:42 +0200

    [CST-4031] Add authorization checks for deleting an impact pathway

--- src/app/impact-pathway-board/shared/impact-path-way/impact-path-way.component.ts
+++ src/app/impact-pathway-board/shared/impact-path-way/impact-path-way.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, Inject, Input, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject, Input, OnInit, ViewChild } from '@angular/core';
 
 import { BehaviorSubject, Observable } from 'rxjs';
 import { NgbAccordion, NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -9,28 +9,41 @@ import { ImpactPathwayLink } from '../../../core/impact-pathway/impact-pathway.r
 import { ImpactPathwayLinksService } from '../../../core/impact-pathway/impact-pathway-links.service';
 import { ImpactPathwayService } from '../../../core/impact-pathway/impact-pathway.service';
 import { ImpactPathwayStep } from '../../../core/impact-pathway/models/impact-pathway-step.model';
+import { AuthorizationDataService } from '../../../core/data/feature-authorization/authorization-data.service';
+import { FeatureID } from '../../../core/data/feature-authorization/feature-id';
+import { mergeMap, take } from 'rxjs/operators';
+import { Item } from '../../../core/shared/item.model';
 
 @Component({
   selector: 'ipw-impact-path-way',
   styleUrls: ['./impact-path-way.component.scss'],
   templateUrl: './impact-path-way.component.html'
 })
-export class ImpactPathWayComponent {
+export class ImpactPathWayComponent implements OnInit {
 
   @Input() public projectId: string;
   @Input() public impactPathway: ImpactPathway;
 
   @ViewChild('accordionRef', { static: false }) wrapper: NgbAccordion;
 
+  canDeleteImpactPathway$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
   canShowRelations: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
   loaded: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   infoShowed: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
 
   constructor(@Inject(NativeWindowService) protected _window: NativeWindowRef,
+              private authorizationService: AuthorizationDataService,
               private cdr: ChangeDetectorRef,
               private impactPathwayService: ImpactPathwayService,
               private impactPathwayLinksService: ImpactPathwayLinksService,
               private modalService: NgbModal) {
+  }
+
+  ngOnInit() {
+    this.impactPathwayService.retrieveObjectItem(this.impactPathway.id).pipe(
+      mergeMap((item: Item) => this.authorizationService.isAuthorized(FeatureID.CanDelete, item.self, undefined)),
+      take(1)
+    ).subscribe((canDelete) => this.canDeleteImpactPathway$.next(canDelete));
   }
 
   ngAfterContentChecked() {
