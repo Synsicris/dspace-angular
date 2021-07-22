ba57edd9990cfc66beb502775575fb6122700a5d
Giuseppe Digilio <giuseppe.digilio@4science.it>
2021-07-22 16:03:25 +0200

    [CST-4356] use ds-view-simple-item-form to display impact-pathway objective details

--- src/app/impact-pathway-board/wrapper-objectives/objective/objective.component.ts
+++ src/app/impact-pathway-board/wrapper-objectives/objective/objective.component.ts
@@ -1,28 +1,40 @@
-import { Component, Input, ViewChild } from '@angular/core';
-import { NgbAccordion, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { NgbAccordion } from '@ng-bootstrap/ng-bootstrap';
 
 import { ImpactPathwayTask } from '../../core/models/impact-pathway-task.model';
 import { ImpactPathwayStep } from '../../core/models/impact-pathway-step.model';
 import { isEmpty } from '../../../shared/empty.util';
 import { ImpactPathwayService } from '../../core/impact-pathway.service';
-import { EditSimpleItemModalComponent } from '../../../shared/edit-simple-item-modal/edit-simple-item-modal.component';
 import { Item } from '../../../core/shared/item.model';
+import { SubmissionFormModel } from '../../../core/config/models/config-submission-form.model';
 
 @Component({
   selector: 'ipw-objective',
   styleUrls: ['./objective.component.scss'],
   templateUrl: './objective.component.html'
 })
-export class ObjectiveComponent {
+export class ObjectiveComponent implements OnInit {
 
   @Input() public projectId: string;
   @Input() public impactPathwayStep: ImpactPathwayStep;
   @Input() public impactPathwayTask: ImpactPathwayTask;
   @Input() public targetImpactPathwayTaskId: string;
 
+  /**
+   * The form config
+   * @type {Observable<SubmissionFormModel>}
+   */
+  formConfig$: Observable<SubmissionFormModel>;
+
   @ViewChild('accordionRef', { static: false }) wrapper: NgbAccordion;
 
-  constructor(private impactPathwayService: ImpactPathwayService, private modalService: NgbModal) {
+  constructor(private impactPathwayService: ImpactPathwayService) {
+  }
+
+  ngOnInit(): void {
+    this.formConfig$ = this.impactPathwayService.getImpactPathwayTaskEditFormConfig(this.impactPathwayStep.type);
   }
 
   isOpen() {
@@ -53,22 +65,13 @@ export class ObjectiveComponent {
     );
   }
 
-  /**
-   * Open dialog box for editing exploitation-plan
-   */
-  openEditModal() {
-    const modalRef = this.modalService.open(EditSimpleItemModalComponent, { size: 'lg' });
-    modalRef.componentInstance.formConfig = this.impactPathwayService.getImpactPathwayTaskEditFormConfig(this.impactPathwayStep.type);
-    modalRef.componentInstance.itemId = this.impactPathwayTask.id;
-
-    modalRef.componentInstance.itemUpdate.subscribe((item: Item) => {
-      this.impactPathwayTask = this.impactPathwayService.updateImpactPathwayTask(item, this.impactPathwayTask);
-      this.impactPathwayService.dispatchUpdateImpactPathwayTask(
-        this.impactPathwayStep.parentId,
-        this.impactPathwayStep.id,
-        this.impactPathwayTask.id,
-        this.impactPathwayTask
-      );
-    });
+  updateImpactPathwayTask(item: Item) {
+    this.impactPathwayTask = this.impactPathwayService.updateImpactPathwayTask(item, this.impactPathwayTask);
+    this.impactPathwayService.dispatchUpdateImpactPathwayTask(
+      this.impactPathwayStep.parentId,
+      this.impactPathwayStep.id,
+      this.impactPathwayTask.id,
+      this.impactPathwayTask
+    );
   }
 }
