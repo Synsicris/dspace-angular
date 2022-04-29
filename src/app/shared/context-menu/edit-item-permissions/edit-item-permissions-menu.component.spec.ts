3978880cae0ef92f2d950851edc1802d03cdf6ca
Rezart Vata <rezart.vata@4science.com>
2022-04-29 18:37:23 +0200

    [CST-5810] Finished functionalities

---
+++ src/app/shared/context-menu/edit-item-permissions/edit-item-permissions-menu.component.spec.ts
@@ -0,0 +1,113 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DSpaceObject } from '../../../core/shared/dspace-object.model';
+import { Item } from '../../../core/shared/item.model';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DSpaceObjectType } from '../../../core/shared/dspace-object-type.model';
+import { EditItemDataService } from '../../../core/submission/edititem-data.service';
+import { EditItemPermissionsMenuComponent } from './edit-item-permissions-menu.component';
+import { EditItem } from '../../../core/submission/models/edititem.model';
+import { createSuccessfulRemoteDataObject$ } from '../../remote-data.utils';
+import { createPaginatedList } from '../../testing/utils.test';
+import { EditItemMode } from '../../../core/submission/models/edititem-mode.model';
+import { TranslateLoaderMock } from '../../mocks/translate-loader.mock';
+import { By } from '@angular/platform-browser';
+import { NotificationsService } from '../../notifications/notifications.service';
+import { NotificationsServiceStub } from '../../testing/notifications-service.stub';
+
+describe('EditItemPermissionsMenuComponent', () => {
+  let component: EditItemPermissionsMenuComponent;
+  let componentAsAny: any;
+  let fixture: ComponentFixture<EditItemPermissionsMenuComponent>;
+
+  let editItemDataService: any;
+  let dso: DSpaceObject;
+  // tslint:disable-next-line:prefer-const
+  let notificationService = new NotificationsServiceStub();
+  const editItemMode: EditItemMode = Object.assign(new EditItemMode(), {
+    name: 'test',
+    label: 'test'
+  });
+
+  const editItem: EditItem = Object.assign(new EditItem(), {
+    modes: createSuccessfulRemoteDataObject$(createPaginatedList([editItemMode]))
+  });
+
+  const noEditItem: EditItem = Object.assign(new EditItem(), {
+    modes: createSuccessfulRemoteDataObject$(createPaginatedList([]))
+  });
+
+  beforeEach(async(() => {
+    dso = Object.assign(new Item(), {
+      id: 'test-item',
+      _links: {
+        self: { href: 'test-item-selflink' }
+      }
+    });
+    editItemDataService = jasmine.createSpyObj('EditItemDataService', {
+      findById: jasmine.createSpy('findById')
+    });
+
+    TestBed.configureTestingModule({
+      declarations: [EditItemPermissionsMenuComponent],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateLoaderMock
+          }
+        }),
+        RouterTestingModule.withRoutes([])],
+      providers: [
+        { provide: EditItemDataService, useValue: editItemDataService },
+        { provide: 'contextMenuObjectProvider', useValue: dso },
+        { provide: 'contextMenuObjectTypeProvider', useValue: DSpaceObjectType.ITEM },
+        { provide: NotificationsService, useValue: notificationService }
+
+      ]
+    }).compileComponents();
+  }));
+
+  describe('when edit modes are available', () => {
+    beforeEach(() => {
+      editItemDataService.findById.and.returnValue(createSuccessfulRemoteDataObject$(editItem));
+      fixture = TestBed.createComponent(EditItemPermissionsMenuComponent);
+      component = fixture.componentInstance;
+      componentAsAny = fixture.componentInstance;
+      component.contextMenuObject = dso;
+      fixture.detectChanges();
+    });
+
+    it('should init edit mode properly', () => {
+      expect(componentAsAny.editModes$.value).toEqual([editItemMode]);
+    });
+
+    it('should render a button', () => {
+      const link = fixture.debugElement.query(By.css('button'));
+      expect(link).not.toBeNull();
+    });
+  });
+
+  describe('when no edit modes are available', () => {
+    beforeEach(() => {
+      editItemDataService.findById.and.returnValue(createSuccessfulRemoteDataObject$(noEditItem));
+      fixture = TestBed.createComponent(EditItemPermissionsMenuComponent);
+      component = fixture.componentInstance;
+      componentAsAny = fixture.componentInstance;
+      component.contextMenuObject = dso;
+      fixture.detectChanges();
+    });
+
+    it('should init edit mode properly', () => {
+      expect(componentAsAny.editModes$.value).toEqual([]);
+    });
+
+    it('should render a button', () => {
+      const link = fixture.debugElement.query(By.css('button'));
+      expect(link).toBeNull();
+    });
+
+  });
+
+});
---
+++ src/app/shared/object-list/grant-badge/grant-badge.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { DSpaceObject } from '../../../core/shared/dspace-object.model';
+import { hasValue, isEmpty, isUndefined } from '../../empty.util';
+import { getResourceTypeValueFor } from '../../../core/cache/object-cache.reducer';
+
+@Component({
+  selector: 'ds-grant-badge',
+  templateUrl: './grant-badge.component.html'
+})
+/**
+ * Component rendering the type of an item as a badge
+ */
+export class GrantBadgeComponent implements OnInit {
+
+  /**
+   * The component used to retrieve the type from
+   */
+  @Input() object: DSpaceObject;
+
+  projectShared: string;
+
+  ngOnInit() {
+    if (!isUndefined(this.object.firstMetadataValue('cris.project.shared')) && hasValue(this.object.firstMetadataValue('cris.project.shared'))) {
+      this.projectShared = this.object.firstMetadataValue('cris.project.shared');
+      console.log(this.object);
+    }
+  }
+
+
+}
