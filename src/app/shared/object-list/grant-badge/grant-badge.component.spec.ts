934081c4e6e17eb8da619f5b00f2457c271e9de3
Giuseppe Digilio <giuseppe.digilio@4science.it>
2022-05-25 13:31:47 +0200

    [CST-5810] Improve badge grant implementation

--- src/app/shared/object-list/grant-badge/grant-badge.component.spec.ts
+++ src/app/shared/object-list/grant-badge/grant-badge.component.spec.ts
@@ -1,11 +1,14 @@
 import { Item } from '../../../core/shared/item.model';
 import { of as observableOf } from 'rxjs';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { TranslateModule } from '@ngx-translate/core';
-import { TruncatePipe } from '../../utils/truncate.pipe';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { ChangeDetectionStrategy, NO_ERRORS_SCHEMA } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { GrantBadgeComponent } from './grant-badge.component';
+import { ProjectDataService } from '../../../core/project/project-data.service';
+import { ProjectGroupService } from '../../../core/project/project-group.service';
+import { TranslateLoaderMock } from '../../mocks/translate-loader.mock';
+import { createSuccessfulRemoteDataObject$ } from '../../remote-data.utils';
 
 let comp: GrantBadgeComponent;
 let fixture: ComponentFixture<GrantBadgeComponent>;
@@ -33,6 +36,12 @@ const mockItemParentProject = Object.assign(new Item(), {
         value: type
       }
     ],
+    'cris.policy.group': [
+      {
+        language: 'en_US',
+        value: 'group'
+      }
+    ],
     'cris.project.shared': [
       {
         language: 'en_US',
@@ -51,6 +60,12 @@ const mockItemProject = Object.assign(new Item(), {
         value: type
       }
     ],
+    'cris.policy.group': [
+      {
+        language: 'en_US',
+        value: 'group'
+      }
+    ],
     'cris.project.shared': [
       {
         language: 'en_US',
@@ -60,11 +75,42 @@ const mockItemProject = Object.assign(new Item(), {
   }
 });
 
+const mockProject = Object.assign(new Item(), {
+  bundles: observableOf({}),
+  metadata: {
+    'dc.title': [
+      {
+        language: 'en_US',
+        value: 'project'
+      }
+    ]
+  }
+});
+
+const projectDataService = jasmine.createSpyObj('ProjectDataService', {
+  getProjectItemByProjectCommunityId: jasmine.createSpy('getProjectItemByProjectCommunityId')
+});
+
+const projectGroupService = jasmine.createSpyObj('ProjectGroupService', {
+  getCommunityIdByGroupName: jasmine.createSpy('getCommunityIdByGroupName')
+});
+
 describe('ItemGrantBadgeComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [TranslateModule.forRoot()],
-      declarations: [GrantBadgeComponent, TruncatePipe],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateLoaderMock
+          }
+        })
+      ],
+      declarations: [GrantBadgeComponent],
+      providers: [
+        { provide: ProjectDataService, useValue: projectDataService },
+        { provide: ProjectGroupService, useValue: projectGroupService },
+      ],
       schemas: [NO_ERRORS_SCHEMA]
     }).overrideComponent(GrantBadgeComponent, {
       set: { changeDetection: ChangeDetectionStrategy.Default }
@@ -110,6 +156,8 @@ describe('ItemGrantBadgeComponent', () => {
   describe('When the item has cris.project.shared project', () => {
     beforeEach(() => {
       comp.object = mockItemProject;
+      projectGroupService.getCommunityIdByGroupName.and.returnValue('groupId');
+      projectDataService.getProjectItemByProjectCommunityId.and.returnValue(createSuccessfulRemoteDataObject$(mockProject));
       fixture.detectChanges();
     });
 
@@ -122,9 +170,6 @@ describe('ItemGrantBadgeComponent', () => {
       const badge = fixture.debugElement.query(By.css('.fa-lock'));
       expect(badge).toBeTruthy();
     });
-
   });
-
-
 
 });
--- src/app/shared/object-list/grant-badge/grant-badge.component.ts
+++ src/app/shared/object-list/grant-badge/grant-badge.component.ts
@@ -1,14 +1,27 @@
 import { Component, Input, OnInit } from '@angular/core';
+
+import { BehaviorSubject } from 'rxjs';
+
 import { DSpaceObject } from '../../../core/shared/dspace-object.model';
-import { hasValue, isEmpty, isUndefined } from '../../empty.util';
-import { getResourceTypeValueFor } from '../../../core/cache/object-cache.reducer';
+import { isNotEmpty } from '../../empty.util';
+import {
+  POLICY_GROUP_METADATA,
+  POLICY_SHARED_METADATA,
+  ProjectDataService,
+  ProjectGrantsTypes
+} from '../../../core/project/project-data.service';
+import { ProjectGroupService } from '../../../core/project/project-group.service';
+import { RemoteData } from '../../../core/data/remote-data';
+import { Item } from '../../../core/shared/item.model';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'ds-grant-badge',
+  styleUrls: ['./grant-badge.component.scss'],
   templateUrl: './grant-badge.component.html'
 })
 /**
- * Component rendering the type of an item as a badge
+ * Component rendering the type of item as a badge
  */
 export class GrantBadgeComponent implements OnInit {
 
@@ -16,12 +29,59 @@ export class GrantBadgeComponent implements OnInit {
    * The component used to retrieve the type from
    */
   @Input() object: DSpaceObject;
+  /**
+   * Containt the badge message to display
+   */
+  badgeMessage: BehaviorSubject<string> = new BehaviorSubject<string>('');
 
+  /**
+   * Contain the policy group name
+   */
+  policyGroup: string;
+
+  /**
+   * Contain the policy shared value
+   */
   projectShared: string;
 
+  /**
+   * Contain the value when policy is open
+   */
+  parentProjectPolicy: string = ProjectGrantsTypes.Parentproject;
+
+  /**
+   * Contain the value when policy is restricted
+   */
+  projectPolicy: string = ProjectGrantsTypes.Subproject;
+
+  constructor(
+    private projectService: ProjectDataService,
+    private projectGroupService: ProjectGroupService,
+    private translate: TranslateService) {
+  }
+
   ngOnInit() {
-    if (!isUndefined(this.object.firstMetadataValue('cris.project.shared')) && hasValue(this.object.firstMetadataValue('cris.project.shared'))) {
-      this.projectShared = this.object.firstMetadataValue('cris.project.shared');
+    if (isNotEmpty(this.object.firstMetadataValue(POLICY_SHARED_METADATA))) {
+      this.projectShared = this.object.firstMetadataValue(POLICY_SHARED_METADATA);
+    }
+
+    if (isNotEmpty(this.object.firstMetadataValue(POLICY_GROUP_METADATA))) {
+      this.policyGroup = this.object.firstMetadataValue(POLICY_GROUP_METADATA);
+    }
+
+    if (isNotEmpty(this.policyGroup)) {
+      if (this.projectShared === ProjectGrantsTypes.Parentproject) {
+        this.badgeMessage.next(this.translate.instant('item.grant.badge.parentproject'));
+        console.log(this.badgeMessage.value);
+      } else {
+        const communityId = this.projectGroupService.getCommunityIdByGroupName(this.policyGroup);
+        this.projectService.getProjectItemByProjectCommunityId(communityId)
+          .subscribe((projectItemRD: RemoteData<Item>) => {
+            const projectItemName = (projectItemRD.hasSucceeded) ? projectItemRD.payload?.name : '';
+            this.badgeMessage.next(this.translate.instant('item.grant.badge.project',{ name: projectItemName } ));
+            console.log(projectItemRD);
+          });
+      }
     }
   }
 
