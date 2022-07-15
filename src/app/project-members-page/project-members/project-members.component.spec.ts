6cf9155ee31330230e3e4cdc7cc36615c6f13f89
Rezart Vata <rezart.vata@atis.al>
2022-07-15 20:56:08 +0200

    [CST-6143] Finished unit testing

--- src/app/project-members-page/project-members/project-members.component.spec.ts
+++ src/app/project-members-page/project-members/project-members.component.spec.ts
@@ -1,3 +1,6 @@
+import { Community } from './../../core/shared/community.model';
+import { NotificationsServiceStub } from './../../shared/testing/notifications-service.stub';
+import { TranslateLoaderMock } from './../../shared/testing/translate-loader.mock';
 import { Group } from './../../core/eperson/models/group.model';
 import { buildPaginatedList, PaginatedList } from './../../core/data/paginated-list.model';
 import { RemoteData } from './../../core/data/remote-data';
@@ -5,17 +8,23 @@ import { GroupMock, GroupMock2 } from './../../shared/testing/group-mock';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectMembersComponent } from './project-members.component';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { GroupDataService } from '../../core/eperson/group-data.service';
 import { PageInfo } from '../../core/shared/page-info.model';
 import { createSuccessfulRemoteDataObject$ } from '../../shared/remote-data.utils';
 import { ProjectGroupService } from '../../core/project/project-group.service';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { NotificationsService } from '../../shared/notifications/notifications.service';
 
 describe('ProjectMembersComponent', () => {
   let component: ProjectMembersComponent;
   let fixture: ComponentFixture<ProjectMembersComponent>;
   let groupsDataServiceStub: any;
   let mockGroups;
+  let notificationService: NotificationsServiceStub;
+  let spyGetGroups;
+  let modalService;
+
 
   const projectGroupService = jasmine.createSpyObj('ProjectGroupService', {
     getInvitationProjectAllGroupsByCommunity: jasmine.createSpy('getInvitationProjectAllGroupsByCommunity'),
@@ -24,8 +33,18 @@ describe('ProjectMembersComponent', () => {
     getInvitationSubprojectMembersGroupsByCommunity: jasmine.createSpy('getInvitationSubprojectMembersGroupsByCommunity'),
   });
 
+  const mockCommunity = Object.assign(new Community(), {
+    id: 'test-uuid',
+    metadata: [
+      {
+        key: 'dc.title',
+        value: 'test community'
+      }
+    ]
+  });
 
   beforeEach(async () => {
+    notificationService = new NotificationsServiceStub();
     mockGroups = [GroupMock, GroupMock2];
     groupsDataServiceStub = {
       allGroups: mockGroups,
@@ -78,13 +97,29 @@ describe('ProjectMembersComponent', () => {
           totalPages: 1,
           currentPage: 1
         }), [result]));
-      }
+      },
+      // tslint:disable-next-line:no-empty
+      editGroup(): void { },
+      getActiveGroup() {
+        return of(GroupMock);
+      },
+      addMemberToGroup: jasmine.createSpy('addMemberToGroup'),
+      deleteMemberFromGroup: jasmine.createSpy('deleteMemberFromGroup'),
     };
     await TestBed.configureTestingModule({
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateLoaderMock
+          }
+        }),
+      ],
       declarations: [ProjectMembersComponent],
       providers: [
         { provide: GroupDataService, useValue: groupsDataServiceStub },
-        { provide: ProjectGroupService, useValue: projectGroupService }
+        { provide: ProjectGroupService, useValue: projectGroupService },
+        { provide: NotificationsService, useValue: notificationService },
       ]
     })
       .compileComponents();
@@ -93,10 +128,46 @@ describe('ProjectMembersComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProjectMembersComponent);
     component = fixture.componentInstance;
+    component.targetGroup = GroupMock;
+    component.relatedCommunity = mockCommunity;
+
+    spyGetGroups = spyOn(component, 'getGroups');
+    spyGetGroups.and.returnValue(of([GroupMock.id, GroupMock2.id]));
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('when sendInvitation is called should call getGroups and modal open', () => {
+    modalService = (component as any).modalService;
+    spyOn(modalService, 'open').and.returnValue(Object.assign({ componentInstance: Object.assign({ response: of(true) }) }));
+    component.sendInvitation('test@test.com');
+    fixture.detectChanges();
+    expect(spyGetGroups).toHaveBeenCalled();
+    expect(modalService.open).toHaveBeenCalled();
+  });
+
+  it('when addMemberToMultipleGroups is called should call getGroupsEntity and addMemberToGroup', () => {
+    spyOn(component, 'getGroupsEntity').and.returnValue(of(mockGroups));
+    groupsDataServiceStub.addMemberToGroup.and.returnValue({});
+    component.addMemberToMultipleGroups({});
+    fixture.detectChanges();
+    expect(component.getGroupsEntity).toHaveBeenCalled();
+    expect(groupsDataServiceStub.addMemberToGroup).toHaveBeenCalled();
+  });
+
+
+  it('when deleteMemberToMultipleGroups is called should call getGroupsEntity and deleteMemberFromGroup', () => {
+    spyOn(component, 'getGroupsEntity').and.returnValue(of(mockGroups));
+    groupsDataServiceStub.deleteMemberFromGroup.and.returnValue({});
+    component.deleteMemberToMultipleGroups({});
+    fixture.detectChanges();
+    expect(component.getGroupsEntity).toHaveBeenCalled();
+    expect(groupsDataServiceStub.deleteMemberFromGroup).toHaveBeenCalled();
+  });
+
+
+
 });
