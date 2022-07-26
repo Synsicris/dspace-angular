1308b7f59983054fd966963ad6bc6c2940f6e1da
Sufiyan Shaikh <sufiyan.shaikh@4science.com>
2022-07-26 17:29:03 +0530

    [CST-6102] test cases fixed

--- src/app/query-builder/query-condition-group/query-condition-group.component.spec.ts
+++ src/app/query-builder/query-condition-group/query-condition-group.component.spec.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormArray, FormBuilder, FormControl, FormGroup, FormGroupDirective, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroupDirective, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BrowserModule, By } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
@@ -15,9 +15,10 @@ import { createSuccessfulRemoteDataObject$ } from '../../shared/remote-data.util
 import { SearchFilterConfig } from '../../shared/search/models/search-filter-config.model';
 import { FilterType } from '../../shared/search/models/filter-type.model';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
-fdescribe('QueryConditionGroupComponent', () => {
-  let component: QueryConditionGroupComponent;
+describe('QueryConditionGroupComponent', () => {
+  let component: any;
   let fixture: ComponentFixture<QueryConditionGroupComponent>;
   let builderService: FormBuilderService;
   let searchService: SearchService;
@@ -33,11 +34,41 @@ fdescribe('QueryConditionGroupComponent', () => {
   });
 
   const searchServiceStub = jasmine.createSpyObj('SearchService', {
-    getConfig: jasmine.createSpy('getConfig')
+    getConfig: jasmine.createSpy('getConfig'),
+    getFacetValuesFor: jasmine.createSpy('getFacetValuesFor')
   });
 
   beforeEach(async () => {
     builderService = getMockFormBuilderService();
+    const formBuilder = new FormBuilder()
+
+    const formGroupDirective = new FormGroupDirective([], []);
+    formGroupDirective.form = formBuilder.group({
+      queryArray: formBuilder.array([
+        formBuilder.group({
+          defaultFilter: formBuilder.control(null),
+          queryGroup: formBuilder.array([
+            formBuilder.group({
+              filter: formBuilder.control(
+                { value: null, disabled: true },
+                Validators.required
+              ),
+              value: formBuilder.control(
+                { value: null, disabled: true },
+                Validators.required
+              ),
+              toDate: formBuilder.control(
+                { value: null, disabled: false },
+              ),
+              fromDate: formBuilder.control(
+                { value: null, disabled: false },
+              ),
+            })
+          ])
+        })
+      ])
+    });
+
     await TestBed.configureTestingModule({
       imports: [
         CommonModule,
@@ -55,11 +86,13 @@ fdescribe('QueryConditionGroupComponent', () => {
       ],
       declarations: [ QueryConditionGroupComponent ],
       providers: [
-        FormGroupDirective,
+        FormBuilder,
+        { provide: FormGroupDirective, useValue: formGroupDirective },
         { provide: SearchService, useValue: searchServiceStub },
         { provide: LocaleService, useValue: {} },
         { provide: FormBuilderService, useValue: builderService },
-      ]
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   });
@@ -67,40 +100,9 @@ fdescribe('QueryConditionGroupComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(QueryConditionGroupComponent);
     component = fixture.componentInstance;
-    searchService = (component as any).searchService;
+    component.formGroupName = 0;
+    searchService = component.searchService;
     searchServiceStub.getConfig.and.returnValue(createSuccessfulRemoteDataObject$([mockFundingFilterConfig, mockDateFilterConfig]));
-    component.formGroup = new FormGroup({
-      defaultFilter: new FormControl(null),
-      queryGroup: new FormArray([(new FormBuilder).group({
-        filter: (new FormBuilder).control(
-          { value: null, disabled: true },
-          Validators.required
-        ),
-        value: (new FormBuilder).control(
-          { value: null, disabled: true },
-          Validators.required
-        ),
-        toDate: (new FormBuilder).control(
-          { value: null, disabled: false },
-        ),
-        fromDate: (new FormBuilder).control(
-          { value: null, disabled: false },
-        ),
-      })]),
-    });
-    (component.formGroup.get('queryGroup') as FormArray).get('0.filter').setValue({
-      "type": {
-          "value": "discovery-filter"
-      },
-      "pageSize": 10,
-      "name": "types",
-      "filterType": "authority",
-      "_links": {
-          "self": {
-              "href": "http://localhost:8080/server/api/discover/facets/types"
-          }
-      }
-    });
     fixture.detectChanges();
   });
 
@@ -108,8 +110,30 @@ fdescribe('QueryConditionGroupComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should show dropdown', () => {
-    expect(fixture.debugElement.query(By.css('div[data-test="query-condition-select"]'))).not.toBeNull();
-    expect(fixture.debugElement.query(By.css('div[data-test="query-condition-date"]'))).toBeNull();
+  it('should show dropdown and do not show datepicker', () => {
+    expect(fixture.debugElement.query(By.css('[data-test="query-condition-select"]'))).not.toBeNull();
+
+    expect(fixture.debugElement.query(By.css('[data-test="query-condition-date"]'))).toBeNull();
+  });
+
+  it('should not show dropdown and show datepicker', () => {
+    component.queryGroup.get('0.filter').setValue({
+      "type": {
+          "value": "projectStartDate"
+      },
+      "pageSize": 10,
+      "name": "types",
+      "filterType": "date",
+      "_links": {
+          "self": {
+              "href": "http://localhost:8080/server/api/discover/facets/types"
+          }
+      }
+    });
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.query(By.css('[data-test="query-condition-select"]'))).toBeNull();
+
+    expect(fixture.debugElement.query(By.css('[data-test="query-condition-date"]'))).not.toBeNull();
   });
 });
