bd6f44c36aace240189acce6e126c2de1b38c9e2
Sufiyan Shaikh <sufiyan.shaikh@4science.com>
2022-07-28 16:53:12 +0530

    [CST-6102] date format fixed

--- src/app/query-builder/query-condition-group/query-condition-group.component.ts
+++ src/app/query-builder/query-condition-group/query-condition-group.component.ts
@@ -663,8 +663,8 @@ export class QueryConditionGroupComponent implements OnInit {
   }
 
   toModel(date: NgbDateStruct): string {
-    return date ? date.year + '/' + ('0' + date.month).slice(-2)
-           + '/' + ('0' + date.day).slice(-2) : null;
+    return date ? date.year + '-' + ('0' + date.month).slice(-2)
+           + '-' + ('0' + date.day).slice(-2) : null;
   }
 }
 
