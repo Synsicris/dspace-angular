db8179d9e799c8749a596b04cf81dc4da09a3f06
lossow <stefan.lossow@.disy.net>
2022-12-19 18:12:43 +0100

    Integration 2022-12-19

---
+++ src/help-pages/de/de-project-use-role.php
@@ -0,0 +1,61 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+<?php
+include("../includes/head-content.inc.php"); echo "\n"; // Angaben im Head (Meta, Stil, Scripte)
+?>
+
+    <title>Titel eintragen (SynSICRIS-Hilfe)</title>
+
+</head>
+
+ <body class="use" id="use-role">
+
+  <?php include("includes/header.inc.php"); echo "\n"; // Header ("SynSICRIS Hilfe") ?>
+
+	 <?php /* Alte Navigation kann geloescht werden.
+	 <h1>&nbsp;</h1>
+	 <!-- Platzhalter deutsch / englisch für Auswahl anderer Sprache -->
+      <n1><a href="de-project-about.php">Über_das_Tool</a></n1><br> <!-- geprueft, unterschiedliche Titel -->
+      <n1><a href="de-project-use.php">Navigation_/_Funktion</a></n1><br> <!-- geprueft, unterschiedliche Titel -->
+	  <n2><strong>Einträge_anlegen</strong></n2><br>
+	  <n2><a href="de-project-use-fill.php">Einträge_ausfüllen</a></n2><br> <!-- Datei fehlt -->
+	  <n2><a href="de-project-use-saving.php">Speichern</a></n2><br> <!-- Datei fehlt -->
+    <n2><a href="de-project-use-permissions.php">Bearbeitungsrechte_festlegen</a></n2><br> <!-- geprueft -->
+	  <n2><a href="de-project-use-versioning.php">Versionierung</a></n2><br>> <!-- geprueft -->
+	  <n2><a href="de-project-use-export.php">Export_/_Drucken</a></n2><br> <!-- geprueft, unterschiedliche Titel -->
+      <n1><a href="de-project-funder.php">Förderinformationen</a></n1><br> <!-- geprueft -->
+	  <n1><a href="de-project-step.php">Nutzung_im_Projektverlauf</a></n1><br> <!-- geprueft, unterschiedliche Titel -->
+	  <n1><a href="de-project-details.php">Details</a></n1> <!-- Datei fehlt -->
+	  */ ?>
+
+  <?php include("includes/sidenav.inc.php"); echo "\n"; // Seitennavigation ?>
+
+	<main>
+      <h1>Rollen und Rechte verwalten</h1>
+
+		<article>
+      Es gibt vier Rollen, die mit unterschiedlich weitreichenden Rechten verbunden sind:
+
+
+</p>
+
+		</article>
+
+    <article>
+  <img src="img/de_project_use_role_Rollen_in_Verbund_und_Projekt.png">
+    </article>
+
+
+    </main>
+
+
+  <footer align="center">
+     <p>Kontakt: <a href="mailto:support@synsicris.de">support@synsicris.de</a> </p>
+     <p>© 2021 by SynSICRIS: Universität Kassel & Disy Informationssysteme GmbH</p>
+  </footer>
+  </body>
+</html>
