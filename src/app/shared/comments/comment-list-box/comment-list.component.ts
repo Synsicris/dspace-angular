62b8b3ca1bdf15a440c1eb1371782681fbe36238
Giuseppe Digilio <giuseppe.digilio@4science.it>
2023-02-17 09:46:59 +0100

    [CST-6221] Use unique id as scope id to retrieve the comment list

--- src/app/shared/comments/comment-list-box/comment-list.component.ts
+++ src/app/shared/comments/comment-list-box/comment-list.component.ts
@@ -1,14 +1,17 @@
 import { LayoutBox } from '../../../cris-layout/enums/layout-box.enum';
 import { isEqual } from 'lodash';
-import {
-  SortDirection,
-  SortOptions,
-} from '../../../core/cache/models/sort-options.model';
+import { SortDirection, SortOptions, } from '../../../core/cache/models/sort-options.model';
 import { PaginationComponentOptions } from '../../pagination/pagination-component-options.model';
 import { PaginatedSearchOptions } from '../../search/models/paginated-search-options.model';
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BrowseMostElementsComponent } from '../../browse-most-elements/browse-most-elements.component';
+import { Item } from '../../../core/shared/item.model';
+import { VERSION_UNIQUE_ID } from '../../../core/project/project-data.service';
+import { hasValue } from '../../empty.util';
+import { ItemDataService } from '../../../core/data/item-data.service';
+import { getFirstCompletedRemoteData } from '../../../core/shared/operators';
+import { RemoteData } from '../../../core/data/remote-data';
 
 @Component({
   selector: 'ds-comment-list',
@@ -29,55 +32,56 @@ export class CommentListComponent implements OnInit {
 
   /**
    * Item's identifier
-  */
- @Input() itemUuid: string;
+   */
+  @Input() itemUuid: string;
 
- /**
-  * Type of plan
- */
-@Input() type: string;
+  /**
+   * Type of plan
+   */
+  @Input() type: string;
 
-/**
- * Flag to define if the card header should be displayd or not
-*/
-@Input() showCardHeader = true;
+  /**
+   * Flag to define if the card header should be displayd or not
+   */
+  @Input() showCardHeader = true;
 
   /**
    * Title of card header
-  */
- @Input() title?: string;
+   */
+  @Input() title?: string;
+
+  /**
+   * the item for which to list comments
+   */
+  item: Item;
 
   /**
    * The PaginatedSearchOptions
-  */
- paginatedSearchOptions: PaginatedSearchOptions;
+   */
+  paginatedSearchOptions: PaginatedSearchOptions;
 
- /**
-  * Reference for configurationSearchPage
- */
-@ViewChild(BrowseMostElementsComponent) configurationSearchPage: BrowseMostElementsComponent;
+  /**
+   * Reference for configurationSearchPage
+   */
+  @ViewChild(BrowseMostElementsComponent) configurationSearchPage: BrowseMostElementsComponent;
 
-constructor(
-  protected route: ActivatedRoute,
+  constructor(
+    protected itemService: ItemDataService,
+    protected route: ActivatedRoute,
   ) {
   }
 
   ngOnInit(): void {
-    const pagination: PaginationComponentOptions = Object.assign(
-      new PaginationComponentOptions(),
-      {
-        id: 'cl',
-        pageSize: 5,
-        currentPage: 1,
-      }
-    );
 
-    this.paginatedSearchOptions = new PaginatedSearchOptions({
-      configuration: this.getConfiguration,
-      pagination: pagination,
-      scope: this.itemUuid,
-      sort: new SortOptions('synsicris.date.creation', SortDirection.DESC),
+    this.itemService.findById(this.itemUuid).pipe(
+      getFirstCompletedRemoteData()
+    ).subscribe((itemRD: RemoteData<Item>) => {
+      if (itemRD.hasSucceeded && !itemRD.hasNoContent) {
+        this.item = itemRD.payload;
+        this.initPaginationOptions(this.item);
+      }
     });
+
   }
 
   /**
@@ -93,9 +97,9 @@ constructor(
     } else if (isEqual(this.type, 'workingplan')) {
       configName = configName.concat('workingplan.comment');
     } else if (isEqual(
-        this.type,
-        'impact_pathway_step_type_2_task_objective_edit_form'
-      )) {
+      this.type,
+      'impact_pathway_step_type_2_task_objective_edit_form'
+    )) {
       configName = configName.concat('proj_objective.proj_objective_comment');
     } else if (
       isEqual(
@@ -113,8 +117,33 @@ constructor(
    * Call the refresh functionality to the reference of the configuration search page
    */
   refresh() {
-    setTimeout(() => {
-      this.configurationSearchPage.refresh();
+    this.configurationSearchPage.refresh();
+  }
+
+  private initPaginationOptions(item: Item) {
+    const pagination: PaginationComponentOptions = Object.assign(
+      new PaginationComponentOptions(),
+      {
+        id: 'cl',
+        pageSize: 5,
+        currentPage: 1,
+      }
+    );
+
+    this.paginatedSearchOptions = new PaginatedSearchOptions({
+      configuration: this.getConfiguration,
+      pagination: pagination,
+      scope: this.getScopeItemId(item),
+      sort: new SortOptions('synsicris.date.creation', SortDirection.DESC),
     });
+  }
+
+  private getScopeItemId(item: Item): string {
+    const uniqueId = item.firstMetadataValue(VERSION_UNIQUE_ID);
+    let scopeId = null;
+    if (!hasValue(uniqueId) || !hasValue(scopeId = uniqueId.split('_')[0])) {
+      return item.id;
+    }
+    return scopeId;
   }
 }
