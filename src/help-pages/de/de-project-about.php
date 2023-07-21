055d86a29ba24b291943957ec1722aa6f27d8121
lossow <stefan.lossow@.disy.net>
2023-07-21 16:52:21 +0200

    Integration 2023-07-21

--- src/help-pages/de/de-project-about.php
+++ src/help-pages/de/de-project-about.php
@@ -34,9 +34,9 @@ include("../includes/head-content.inc.php"); echo "\n"; // Angaben im Head (Meta
 <h1>Das Monitoring-Tool</h1>
 
     <h2>Worum es im Monitoring-Tool geht:</h2>
-    <p>Das SynSICRIS-Monitoring-Tool dient dazu, Hintergründe, Ziele, Arbeiten, Ergebnisse und Wirkungspotenziale eines Verbundes oder Einzelprojektes zu erfassen. Teile von Textdokumenten für Anträge und Berichte für den Fördermittelgeber sollen dadurch ersetzt werden. Anstatt zusammenhängender Textdokumente erstellen Sie einzelne „Einträge“ (z. B. ein Ziel oder eine Veröffentlichung). Diese Einträge können strukturiert werden, indem Sie sie in einem Wirkungspfad, in Ihrem Arbeitsplan und im Verwertungsplan wie an einer Pinnwand anpinnen.</p>
+    <p>Das SynSICRIS-Monitoring-Tool dient dazu, Hintergründe, Ziele, Arbeiten, Ergebnisse und Wirkungspotenziale eines Verbundes oder Einzelprojektes zu erfassen. Teile von Textdokumenten für <strong>Anträge und Berichte</strong> für den Fördermittelgeber <strong> sollen dadurch ersetzt</strong> werden. Anstatt zusammenhängender Textdokumente erstellen Sie <strong>einzelne „Einträge“</strong> (z. B. ein Ziel oder eine Veröffentlichung). Diese Einträge können strukturiert werden, indem Sie sie in einem Wirkungspfad, in Ihrem Arbeitsplan und im Verwertungsplan wie an einer Pinnwand anpinnen.</p>
     <p>Sie erstellen und bearbeiten die Einträge zusammen mit allen Projektpartnern.</p>
-    <p>Das Tool soll Sie von der Antragstellung bis 2 Jahre nach Projektende begleiten, indem Sie die Einträge regelmäßig aktualisieren und ergänzen. Für die Antragstellung und zu allen Berichtszeitpunkten erstellen Sie eine Version für den Fördermittelgeber, in welcher er die Einträge lesen kann.  Außerdem können Sie die Informationen in PDF-Dokumente exportieren.</p>
+    <p>Das Tool soll Sie von der <strong>Antragstellung bis 2 Jahre nach Projektende</strong> begleiten, indem Sie die Einträge regelmäßig aktualisieren und ergänzen. Für die Antragstellung und zu allen Berichtszeitpunkten erstellen Sie eine <strong>Version für den Fördermittelgeber</strong>, in welcher er die Einträge lesen kann.  Außerdem können Sie die Informationen in PDF-Dokumente exportieren.</p>
 
 
     </main>
